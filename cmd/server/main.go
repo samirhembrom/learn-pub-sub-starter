8677fa8d18fdc6db95372be5fdde339806f61c4c
samirhembrom <samirhembrom007@gmail.com>
2025-09-26 01:19:31 +0530

    ch3:l1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,9 @@ import (
 	"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 const connUrl = "amqp://guest:guest@localhost:5672/"
@@ -20,6 +23,17 @@ func main() {
 	defer conn.Close()
 
 	log.Printf("Connention was successful")
+
+	ch, err := conn.Channel()
+	pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,30 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	jsonData, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	ctx := context.Background()
+	err = ch.PublishWithContext(
+		ctx,
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{ContentType: "application/json", Body: jsonData},
+	)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	return nil
+}
