afb580033c7769be88b51c50e943f218391b3472
samirhembrom <samirhembrom007@gmail.com>
2025-10-05 02:19:37 +0530

    ch3:l5

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+)
+
+const connUrl = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	conn, err := amqp.Dial(connUrl)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer conn.Close()
+
+	log.Printf("Connention was successful")
+
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		"pause."+name,
+		routing.PauseKey,
+		1,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	log.Printf("Connection is closing")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -28,3 +28,44 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 
 	return nil
 }
+
+type SimpleQueueType int
+
+const (
+	durable SimpleQueueType = iota
+	transient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := true
+	autoDelete := true
+	exclusive := true
+	noWait := false
+	switch queueType {
+	case 0:
+		autoDelete = false
+		exclusive = false
+	case 1:
+		durable = false
+	}
+	que, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, noWait, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	err = ch.QueueBind(que.Name, key, exchange, noWait, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return ch, que, nil
+}
